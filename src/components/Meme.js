cd8f0091f518327b77a156ac86d66c5826a5aa4f
Youngwoo <youngwoo206@gmail.com>
2022-10-20 20:08:34 -0400

    alt img tag

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -55,7 +55,7 @@ export default function Meme() {
         </button>
       </div>
       <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
+        <img src={meme.randomImage} className="meme--image" alt="meme" />
         <h2 className="meme--text top">{meme.topText}</h2>
         <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
